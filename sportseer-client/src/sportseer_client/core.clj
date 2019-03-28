3f22521f05b6b800cec34b774e269aed45c3e3d5
Scott Taylor <mrscottytaylor@gmail.com>
2019-03-28 19:28:24 +0000

    Main Menu Skeleton pt. 2
    Button text on the main buttons now turns orange when selected.
    
    Method created that resizes the window back to a max width after pack! is used, need to make it so 800 is the max height though.
    
    The skeleton for the training menu has been started.

--- sportseer-client/src/sportseer_client/core.clj
+++ sportseer-client/src/sportseer_client/core.clj
@@ -4,19 +4,40 @@
 
 (use 'seesaw.core)
 
+;; _____________________________________________________________________________________________________________________
+;; Global variables
+
 (def transparent (seesaw.color/color 0 0 0 0))
+(def default-text-colour
+  (let [colour (seesaw.color/default-color "Label.foreground")]
+    (seesaw.color/color (.getRed colour) (.getGreen colour) (.getBlue colour))))
+(def sportseer-orange (seesaw.color/color 255 127 42 255))
+
+;; _____________________________________________________________________________________________________________________
+;; Initial Frame Set Up
 
 (native!)
 (def f (frame :title "SPORTSEER - Sports Match Prediction Client" :resizable? false
               :icon (seesaw.icon/icon (ImageIO/read (clojure.java.io/file "sportseer-logo.png")))))
-(config! f :size [800 :by 800])
 
+;; _____________________________________________________________________________________________________________________
+;; Skeleton Temps
+
+;; Dataset Menu Temp listbox
 (def seasons-list ["NBA Season 2010-2011", "NBA Season 2011-2012", "NBA Season 2012-2013",
                    "NBA Season 2013-2014", "NBA Season 2014-2015", "NBA Season 2015-2016",
                    "NBA Season 2016-2017", "NBA Season 2017-2019 (incomplete)"])
-
 (def temp-seasons-list (listbox :model (-> seasons-list sort)))
-(selection temp-seasons-list  {:multi? true})
+(selection temp-seasons-list {:multi? true})
+
+(def datasets-list ["NBA 10-17 [match-0 minutes-weighted]"
+                    "NBA 10-17 [minutes-weighted]"
+                    "NBA 16-17 [match-0 minutes-weighted]"])
+(def temp-datasets-list (listbox :model (-> datasets-list sort)))
+(selection temp-datasets-list {:multi? true})
+
+;; _____________________________________________________________________________________________________________________
+;; Dataset Menu
 
 (def dataset-menu-buttons
   (let [bg (button-group)]
@@ -33,31 +54,70 @@
                 :south dataset-menu-buttons
                 :vgap 5 :hgap 5))
 
+;; _____________________________________________________________________________________________________________________
+;; Training Menu
 
+(def training-menu-buttons
+  (let [bg (button-group)]
+    (flow-panel :items [(button :text "Create New Model")
+                        (button :text "Train Existing Model")])))
 
-(defn click-and-key-listen [widget function]
-  (listen widget :mouse-clicked function :key-pressed function))
+(def training-menu
+  (border-panel :id "training-menu"
+                :center training-menu-buttons))
+
+;; _____________________________________________________________________________________________________________________
+;; Main Menu Functionality
+
+(defn reset-main-menu-buttons [e]
+  ;this is optimal - but it dont work :@
+  #_(map #(config! % :foreground default-text-colour) (select e [:<javax.swing.JButton>>]))
+  (config! (select f [:#scrape-button]) :foreground default-text-colour)
+  (config! (select f [:#dataset-button]) :foreground default-text-colour)
+  (config! (select f [:#train-button]) :foreground default-text-colour)
+  (config! (select f [:#predict-button]) :foreground default-text-colour))
+
+(defn set-selected-colour [e]
+  (config! e :foreground sportseer-orange))
 
 (declare change-menu)
 (def main-menu-buttons
   (let [bg (button-group)]
-    (flow-panel :items [(button :id "scrape-button" :class :type :text "Scraper" :group bg :mnemonic \s
-                                :listen [:action (fn [e] (change-menu (label :text "Scrape Menu")))])
-                        (button :id "dataset-button" :class :type :text "Datasets" :group bg :mnemonic \d
-                                :listen [:action (fn [e] (change-menu dataset-menu))])
-                        (button :id "train-button" :class :type :text "Models" :group bg :mnemonic \m
-                                :listen [:action (fn [e] (change-menu (label :text "Training Menu")))])
-                        (button :id "predict-button" :class :type :text "Predictions" :group bg :mnemonic \p
-                                :listen [:action (fn [e] (change-menu (label :text "Prediction Menu")))])])))
+    (flow-panel :items [(button :id "scrape-button" :class :type :group bg :mnemonic \s
+                                :text "Data Scraper"
+                                :listen [:action #(do (change-menu (label :text "Scrape Menu"))
+                                                      (reset-main-menu-buttons %) (set-selected-colour %))])
+                        (button :id "dataset-button" :class :type :group bg :mnemonic \d
+                                :text "Datasets"
+                                :listen [:action #(do (change-menu dataset-menu)
+                                                      (reset-main-menu-buttons %) (set-selected-colour %))])
+                        (button :id "train-button" :class :type :group bg :mnemonic \m
+                                :text "Models"
+                                :listen [:action #(do (change-menu training-menu)
+                                                      (reset-main-menu-buttons %) (set-selected-colour %))])
+                        (button :id "predict-button" :class :type :group bg :mnemonic \p
+                                :text "Predictions"
+                                :listen [:action #(do (change-menu (label :text "Prediction Menu"))
+                                                      (reset-main-menu-buttons %) (set-selected-colour %))])])))
 
 (def main-menu (border-panel :north (separator) :center main-menu-buttons :south (separator)))
 (def title-logo
   (label :icon (seesaw.icon/icon (ImageIO/read (clojure.java.io/file "sportseer-logo-text-half.png")))))
 
+(defn max-width-pack! [f]
+  (do (pack! f)
+      (config! f :size [584 :by (.height (.getSize f))])
+      f))
+
 (defn change-menu [content]
   (config! f :content (border-panel :north title-logo :center main-menu :south content
                                     :vgap 10 :hgap 5 :border 15))
-  (pack! f))
+  (-> f max-width-pack!))
 
-(change-menu (label :text "placeholder"))
-(-> f pack! show!)
+(change-menu (styled-text :text (str "This is a client for creating and managing "
+                                     "machine learning models"
+                                     "designed to predict sports match outcomes.\n\n"
+                                     "Created by Scott Taylor\n"
+                                     "For my BSc Computer Science Final Year Project at Teesside University")
+                          :wrap-lines? true :background transparent))
+(-> f max-width-pack! show!)
