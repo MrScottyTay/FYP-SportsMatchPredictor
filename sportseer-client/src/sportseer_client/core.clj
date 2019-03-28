379dfc9b46f3fe9e0d64f8d29cdbecfd05255cd7
Scott Taylor <mrscottytaylor@gmail.com>
2019-03-28 15:51:59 +0000

    Main Menu Skeleton pt. 1
    The main menu buttons have been added as a ribbon just below the title logo.
    Each button then changes the main content of the window to change to that specific sub menu.
    Only the Dataset skeleton with dummy data has been implemented so far.

--- sportseer-client/src/sportseer_client/core.clj
+++ sportseer-client/src/sportseer_client/core.clj
@@ -1,82 +1,63 @@
 (ns sportseer-client.core
-  ;(:import (javax.swing JOptionPane JButton JPanel JFrame JComponent)
-  ;         (java.awt.event ActionListener)
-  ;         (java.awt GridLayout Graphics GridBagLayout)
-  ;         (javax.imageio ImageIO))
   (:require [seesaw.core])
   (:import (javax.imageio ImageIO)))
 
-;(defn say-hello []
-;  (JOptionPane/showMessageDialog
-;    nil "Hello, World!" "Greeting" JOptionPane/INFORMATION_MESSAGE))
-;
-;(def act (proxy [ActionListener] []
-;           (actionPerformed [event] (say-hello))))
-;
-;(def button (doto (JButton. "Click Me!")
-;              (.addActionListener act)))
-;
-;(def logo-panel (proxy [JPanel] []
-;                  (paintComponent [g]
-;                    (proxy-super paintComponent g)
-;                    (.drawImage g (ImageIO/read (clojure.java.io/file "sportseer-logo-text.png")) 0 0 552 93 nil))))
-;
-;(def panel (doto (JPanel.)
-;             (.setLayout (GridBagLayout.))
-;             (.add logo-panel)
-;             (.add button)))
-;(def frame (doto (JFrame. "SportSeer")
-;             (.setSize 800 800)
-;             (.setContentPane panel)
-;             (.setVisible true)))
+(use 'seesaw.core)
+
+(def transparent (seesaw.color/color 0 0 0 0))
 
 (native!)
-(def f (frame :title "SportSeer" :resizable? false
+(def f (frame :title "SPORTSEER - Sports Match Prediction Client" :resizable? false
               :icon (seesaw.icon/icon (ImageIO/read (clojure.java.io/file "sportseer-logo.png")))))
 (config! f :size [800 :by 800])
-
-(defn display [content]
-  (config! f :content content)
-  content)
 
 (def seasons-list ["NBA Season 2010-2011", "NBA Season 2011-2012", "NBA Season 2012-2013",
                    "NBA Season 2013-2014", "NBA Season 2014-2015", "NBA Season 2015-2016",
                    "NBA Season 2016-2017", "NBA Season 2017-2019 (incomplete)"])
 
-(def lb (listbox :model (-> seasons-list sort)))
-(selection lb {:multi? true})
-(display (scrollable lb))
+(def temp-seasons-list (listbox :model (-> seasons-list sort)))
+(selection temp-seasons-list  {:multi? true})
 
-(def rbs (for [i [:source :doc]]
-           (radio :id i :class :type :text (name i))))
+(def dataset-menu-buttons
+  (let [bg (button-group)]
+    (flow-panel :items [(button :text "Change Data Folder")
+                        (button :id "dataset-start-button" :class :type :text "Create Dataset"
+                                :size [415 :by 31])])))
 
-(def field (display (text "This is a text field")))
+(def dataset-menu
+  (border-panel :north (styled-text :text (str "Select the scraped data to use when creating a dataset.\n"
+                                           "Multiple can be selected by Shift and Control Clicking.")
+                                    :wrap-lines? true
+                                    :background transparent)
+                :center (scrollable temp-seasons-list) ; lb is temporary
+                :south dataset-menu-buttons
+                :vgap 5 :hgap 5))
 
+
+
+(defn click-and-key-listen [widget function]
+  (listen widget :mouse-clicked function :key-pressed function))
+
+(declare change-menu)
+(def main-menu-buttons
+  (let [bg (button-group)]
+    (flow-panel :items [(button :id "scrape-button" :class :type :text "Scraper" :group bg :mnemonic \s
+                                :listen [:action (fn [e] (change-menu (label :text "Scrape Menu")))])
+                        (button :id "dataset-button" :class :type :text "Datasets" :group bg :mnemonic \d
+                                :listen [:action (fn [e] (change-menu dataset-menu))])
+                        (button :id "train-button" :class :type :text "Models" :group bg :mnemonic \m
+                                :listen [:action (fn [e] (change-menu (label :text "Training Menu")))])
+                        (button :id "predict-button" :class :type :text "Predictions" :group bg :mnemonic \p
+                                :listen [:action (fn [e] (change-menu (label :text "Prediction Menu")))])])))
+
+(def main-menu (border-panel :north (separator) :center main-menu-buttons :south (separator)))
 (def title-logo
-  (label :icon (seesaw.icon/icon (ImageIO/read (clojure.java.io/file "sportseer-logo-text-half.png")))
-         :size [552 :by 94]))
+  (label :icon (seesaw.icon/icon (ImageIO/read (clojure.java.io/file "sportseer-logo-text-half.png")))))
 
-(def season-selection (border-panel
-                        :north (border-panel
-                                 :north (label "Model Training")
-                                 :center (separator)
-                                 :vgap 5)
-                        :center (border-panel
-                                  :north (border-panel
-                                           :west (button :text "Scrape More")
-                                           :east (button :text "Select Data Folder"))
-                                  :south (scrollable lb)
-                                  :vgap 3)
-                        :south (button :text "Train Model")
-                        :vgap 5 :hgap 5))
+(defn change-menu [content]
+  (config! f :content (border-panel :north title-logo :center main-menu :south content
+                                    :vgap 10 :hgap 5 :border 15))
+  (pack! f))
 
-(def main-panel (border-panel :west season-selection :east field))
-
-(def container (border-panel :north title-logo :south main-panel
-                             :vgap 5 :hgap 5 :border 10))
-
-
-
-(display container)
-
+(change-menu (label :text "placeholder"))
 (-> f pack! show!)
