5c6c203f07ae5e290707ae2cb7f85902d587de80
Scott Taylor <mrscottytaylor@gmail.com>
2019-03-29 08:58:41 +0000

    Main Menu Skeleton pt. 3
    The Train Models menu now has a sub-menu system similar to the main menu, with orange-coloured selections to signify the menu you're currently in.
    
    New model sub menu within the model menu now has a skeleton.
    
    All current buttons now have mnemonics.

--- sportseer-client/src/sportseer_client/core.clj
+++ sportseer-client/src/sportseer_client/core.clj
@@ -14,6 +14,13 @@
 (def sportseer-orange (seesaw.color/color 255 127 42 255))
 
 ;; _____________________________________________________________________________________________________________________
+;; Helper Functions
+
+(defn set-selected-colour [e]
+  (config! e :foreground sportseer-orange))
+
+(declare change-menu)
+;; _____________________________________________________________________________________________________________________
 ;; Initial Frame Set Up
 
 (native!)
@@ -34,22 +41,24 @@
                     "NBA 10-17 [minutes-weighted]"
                     "NBA 16-17 [match-0 minutes-weighted]"])
 (def temp-datasets-list (listbox :model (-> datasets-list sort)))
-(selection temp-datasets-list {:multi? true})
+(selection temp-datasets-list {:multi? false})
 
 ;; _____________________________________________________________________________________________________________________
 ;; Dataset Menu
 
 (def dataset-menu-buttons
   (let [bg (button-group)]
-    (flow-panel :items [(button :text "Change Data Folder")
-                        (button :id "dataset-start-button" :class :type :text "Create Dataset"
+    (flow-panel :items [(button :id "change-data-folder-button" :class :type :group bg :mnemonic \f
+                                :text "Change Data Folder")
+                        (button :id "dataset-start-button" :class :type :group bg :mnemonic \c
+                                :text "Create Dataset"
                                 :size [415 :by 31])])))
 
 (def dataset-menu
   (border-panel :north (styled-text :text (str "Select the scraped data to use when creating a dataset.\n"
                                            "Multiple can be selected by Shift and Control Clicking.")
-                                    :wrap-lines? true
-                                    :background transparent)
+                                    :wrap-lines? true :enabled? false
+                                    :background transparent :foreground default-text-colour)
                 :center (scrollable temp-seasons-list) ; lb is temporary
                 :south dataset-menu-buttons
                 :vgap 5 :hgap 5))
@@ -57,14 +66,54 @@
 ;; _____________________________________________________________________________________________________________________
 ;; Training Menu
 
+(def new-model-buttons
+  (let [bg (button-group)]
+    (flow-panel :items [(button :id "change-dataset-folder-button" :class :type :group bg :mnemonic \f
+                                :text "Change Dataset Folder")
+                        (button :id "new-start-button" :class :type :group bg :mnemonic \c
+                                :text "Create New Model"
+                                :size [400 :by 31])])))
+
+(def create-new-model-menu
+  (border-panel :north (styled-text :text "Select the dataset you want to use on the new model."
+                                    :wrap-lines? true :enabled? false
+                                    :background transparent :foreground default-text-colour)
+                :center (scrollable temp-datasets-list)
+                :south new-model-buttons))
+
+(defn reset-training-menu-buttons [e]
+  (config! (select f [:#new-model-button]) :foreground default-text-colour)
+  (config! (select f [:#train-model-button]) :foreground default-text-colour))
+
+(declare training-menu)
 (def training-menu-buttons
   (let [bg (button-group)]
-    (flow-panel :items [(button :text "Create New Model")
-                        (button :text "Train Existing Model")])))
+    (flow-panel :items [(button :id "new-model-button" :class :type :group bg :mnemonic \n
+                                :text "Create New Model"
+                                :listen [:action #(do (change-menu (training-menu create-new-model-menu))
+                                                    (reset-training-menu-buttons %) (set-selected-colour %))])
+                        (button :id "train-model-button" :class :type :group bg :mnemonic \t
+                                :text "Train Existing Model"
+                                :listen [:action #(do (change-menu (training-menu (label "Placeholder")))
+                                                      (reset-training-menu-buttons %) (set-selected-colour %))])])))
 
-(def training-menu
-  (border-panel :id "training-menu"
-                :center training-menu-buttons))
+(def training-menu-default
+  (styled-text :text "Choose whether to create a new model or to train an existing model."
+               :wrap-lines? true :enabled? false :background transparent :foreground default-text-colour))
+
+(defn training-menu
+  ([] (training-menu training-menu-default))
+  ([sub-menu] (border-panel :id "training-menu"
+                            :north training-menu-buttons
+                            :center (separator)
+                            :south sub-menu)))
+
+;; _____________________________________________________________________________________________________________________
+;; Scraper Menu
+
+(def scraper-menu
+  (styled-text :text "The Scraper has not yet been integrated into the GUI client."
+               :wrap-lines? true :enabled? false :background transparent :foreground default-text-colour))
 
 ;; _____________________________________________________________________________________________________________________
 ;; Main Menu Functionality
@@ -77,15 +126,11 @@
   (config! (select f [:#train-button]) :foreground default-text-colour)
   (config! (select f [:#predict-button]) :foreground default-text-colour))
 
-(defn set-selected-colour [e]
-  (config! e :foreground sportseer-orange))
-
-(declare change-menu)
 (def main-menu-buttons
   (let [bg (button-group)]
     (flow-panel :items [(button :id "scrape-button" :class :type :group bg :mnemonic \s
                                 :text "Data Scraper"
-                                :listen [:action #(do (change-menu (label :text "Scrape Menu"))
+                                :listen [:action #(do (change-menu scraper-menu)
                                                       (reset-main-menu-buttons %) (set-selected-colour %))])
                         (button :id "dataset-button" :class :type :group bg :mnemonic \d
                                 :text "Datasets"
@@ -93,7 +138,7 @@
                                                       (reset-main-menu-buttons %) (set-selected-colour %))])
                         (button :id "train-button" :class :type :group bg :mnemonic \m
                                 :text "Models"
-                                :listen [:action #(do (change-menu training-menu)
+                                :listen [:action #(do (change-menu (training-menu))
                                                       (reset-main-menu-buttons %) (set-selected-colour %))])
                         (button :id "predict-button" :class :type :group bg :mnemonic \p
                                 :text "Predictions"
@@ -111,7 +156,7 @@
 
 (defn change-menu [content]
   (config! f :content (border-panel :north title-logo :center main-menu :south content
-                                    :vgap 10 :hgap 5 :border 15))
+                                    :vgap 5 :hgap 5 :border 15))
   (-> f max-width-pack!))
 
 (change-menu (styled-text :text (str "This is a client for creating and managing "
@@ -119,5 +164,5 @@
                                      "designed to predict sports match outcomes.\n\n"
                                      "Created by Scott Taylor\n"
                                      "For my BSc Computer Science Final Year Project at Teesside University")
-                          :wrap-lines? true :background transparent))
+                          :wrap-lines? true :enabled? false :background transparent :foreground default-text-colour))
 (-> f max-width-pack! show!)
